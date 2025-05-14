f11e049ddc59747ef7816b6f9aa705c67575c270
aaaspblo <a.aspblom@gmail.com>
2025-05-14 15:33:59 +0300

    Remove http:// from domain name

--- tt/classes/com/oddlabs/tt/global/Globals.java
+++ tt/classes/com/oddlabs/tt/global/Globals.java
@@ -3,7 +3,7 @@ package com.oddlabs.tt.global;
 import org.lwjgl.opengl.*;
 
 public final strictfp class Globals {
-	public final static String DOMAIN_NAME = "http://domain.local";
+	public final static String DOMAIN_NAME = "domain.local";
 	public final static String SUPPORT_ADDRESS = "http://"+ DOMAIN_NAME +"/support";
 
 	public final static int BOUNDING_NONE = 0;
