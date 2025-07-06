7c83e741434211a2ebc714680918c469bd73b2d1
Omar Mokhtar <omar.ali.mokhtar@gmail.com>
2025-07-06 11:53:39 +0200

    Packing as AppImage for Linux

--- tt/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export SCRIPT_DIR=$(realpath $(dirname $0))
-
-cd $SCRIPT_DIR/../common
-
-java -ea -Djava.library.path=${SCRIPT_DIR} -Xmx1G -Djdk.crypto.KeyAgreement.legacyKDF=true -cp ".:*" com.oddlabs.tt.Main
