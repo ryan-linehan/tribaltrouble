df151a57badc2f851b01991aa4a76b21c4572716
Omar Mokhtar <omar.ali.mokhtar@gmail.com>
2025-07-06 08:50:04 +0200

    Fixing the Linux launcher `start.sh`

--- tt/start.sh
+++ tt/start.sh
@@ -1,5 +1,7 @@
-export nativelib = $(pwd)
+#!/bin/bash
 
-cd ../common
+export SCRIPT_DIR=$(realpath $(dirname $0))
 
-java -ea -Djava.library.path=${nativelib} -Xmx1G -Djdk.crypto.KeyAgreement.legacyKDF=true -cp ".:*" com.oddlabs.tt.Main
+cd $SCRIPT_DIR/../common
+
+java -ea -Djava.library.path=${SCRIPT_DIR} -Xmx1G -Djdk.crypto.KeyAgreement.legacyKDF=true -cp ".:*" com.oddlabs.tt.Main
