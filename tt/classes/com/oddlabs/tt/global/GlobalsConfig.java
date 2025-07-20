18b802df78abab61278b4fc3eeeb4df368c2ad6b
aaaspblo <a.aspblom@gmail.com>
2025-07-21 00:05:09 +0300

    Hack to add refreshrate, resolution and fullscreen to config.properties

--- tt/classes/com/oddlabs/tt/global/GlobalsConfig.java
+++ tt/classes/com/oddlabs/tt/global/GlobalsConfig.java
@@ -25,4 +25,24 @@ public class GlobalsConfig {
         String domainName = get("DOMAIN_NAME");
         return domainName == null ? "domain.local" : domainName;
     }
+
+    public static boolean isFullscreen() {
+        String fullscreen = get("FULLSCREEN");
+        return fullscreen != null && Boolean.parseBoolean(fullscreen);
+    }
+
+    public static int getWindowWidth() {
+        String width = get("WINDOW_WIDTH");
+        return width != null ? Integer.parseInt(width) : 1920; // Default width
+    }
+
+    public static int getWindowHeight() {
+        String height = get("WINDOW_HEIGHT");
+        return height != null ? Integer.parseInt(height) : 1080; // Default height
+    }
+
+    public static int getRefreshRate() {
+        String refreshRate = get("REFRESH_RATE");
+        return refreshRate != null ? Integer.parseInt(refreshRate) : 60; // Default refresh rate
+    }
 }
--- tt/classes/com/oddlabs/tt/render/Display.java
+++ tt/classes/com/oddlabs/tt/render/Display.java
@@ -10,6 +10,7 @@ import static org.lwjgl.opengl.GL11.*;
 
 import com.oddlabs.tt.input.Keyboard;
 import com.oddlabs.tt.input.Mouse;
+import com.oddlabs.tt.global.GlobalsConfig;
 
 public final strictfp class Display {
     private static long window;
@@ -17,7 +18,9 @@ public final strictfp class Display {
     private static String title = "Tribal Trouble";
     private static int width = 1920;
     private static int height = 1080;
-    
+    private static int refreshRate = 60;
+    private static boolean fullscreen = false;
+
     public static boolean isCreated() {
         return created;
     }
@@ -31,24 +34,30 @@ public final strictfp class Display {
             System.out.println("Unable to initialize GLFW");
             return;
         }
+
+        width = GlobalsConfig.getWindowWidth();
+        height = GlobalsConfig.getWindowHeight();
+        fullscreen = GlobalsConfig.isFullscreen();
+        refreshRate = GlobalsConfig.getRefreshRate();
         
         long monitor = GLFW.glfwGetPrimaryMonitor();
         GLFWVidMode videoMode = GLFW.glfwGetVideoMode(monitor);
 
-        GLFW.glfwWindowHint(GLFW.GLFW_REFRESH_RATE, videoMode.refreshRate());
+        GLFW.glfwWindowHint(GLFW.GLFW_REFRESH_RATE, refreshRate);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
         
        /* GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
         GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);*/
 
-        // TODO: Consider using different resolutions when OpenGL is rewritten.
-        // The game only works with a specific resolution for now.
-        // We can't just use the current one from the system.
-        width = videoMode.width();
-        height = videoMode.height();
-
-        window = GLFW.glfwCreateWindow(width, height, title, monitor, 0);
+        if (fullscreen) {
+            GLFW.glfwWindowHint(GLFW.GLFW_DECORATED, GLFW.GLFW_FALSE);
+            window = GLFW.glfwCreateWindow(width, height, title, monitor, 0);
+        } else {
+            GLFW.glfwWindowHint(GLFW.GLFW_DECORATED, GLFW.GLFW_TRUE);
+            window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
+        }
+        
 
         GLFW.glfwSetInputMode(window, GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_HIDDEN);
         GLFW.glfwShowWindow(window);
