613e2d2dabd9d0cb812c5f01bc1aa7cd4159e723
RcubDev <rlinehan2@gmail.com>
2025-07-26 17:32:50 -0700

    add integration for discord to tribal trouble chatroom chatting

--- server/classes/com/oddlabs/matchserver/ChatRoom.java
+++ server/classes/com/oddlabs/matchserver/ChatRoom.java
@@ -1,107 +1,183 @@
 package com.oddlabs.matchserver;
 
 import com.oddlabs.matchmaking.MatchmakingServerInterface;
+
+import discord4j.common.util.Snowflake;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.TextChannel;
+import reactor.core.publisher.Mono;
+
 import com.oddlabs.matchmaking.MatchmakingClientInterface;
 import com.oddlabs.matchmaking.ChatRoomUser;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public final strictfp class ChatRoom {
-	private final static Map chat_rooms = new HashMap();
-	
-	private final Set users = new HashSet();
-	private final String name;
 
-	public ChatRoom(String name) {
-		this.name = name;
-	}
+    private final static Map<String, ChatRoom> chat_rooms = new HashMap();
 
-	public final static Map getChatRooms() {
-		return chat_rooms;
-	}
+    private final Set<Client> users = new HashSet();
+    private final String name;
+    private TextChannel discordChannel = null;
 
-	public final static void joinStandardChatRoom(Client client) {
-		int room_postfix = 0;
-		while (true) {
-			room_postfix++;
-			ChatRoom room = getChatRoom("Chatroom" + room_postfix);
-			if (room.getUsers().size() > MatchmakingServerInterface.MAX_ROOM_USERS/2) {
-				// skip room
-				continue;
-			}
-			client.joinRoom(room.getName());
-			return;
-		}
-	}
+    public ChatRoom(String name) {
+        this.name = name;
+        discordChannel = DiscordBotService.getInstance().getDiscordChannelByTTRoomNumber(Integer.parseInt(name.substring("Chatroom".length())));
+        if (discordChannel == null) {
+            System.err.println("Discord channel for " + name + " not found!");
+        } else {
+            discordChannel.getClient().on(MessageCreateEvent.class, this::handleIncomingDiscordMessage).subscribe();
+            System.out.println("Discord channel for " + name + " found: " + (discordChannel).getName());
+        }
+    }
 
-	public final static ChatRoom getChatRoom(String room_name) {
-		ChatRoom room = (ChatRoom)chat_rooms.get(room_name);
-		if (room == null) {
-			room = new ChatRoom(room_name);
-			chat_rooms.put(room_name, room);
-		}
-		return room;
-	}
+    public final static Map getChatRooms() {
+        return chat_rooms;
+    }
 
-	public final static boolean isNameValid(String name) {
-		return name != null && name.length() <= MatchmakingServerInterface.MAX_ROOM_NAME_LENGTH
-			&& name.length() >= MatchmakingServerInterface.MIN_ROOM_NAME_LENGTH && areCharactersValid(name);
-	}
-	
-	private final static boolean areCharactersValid(String name) {
-		for (int i = 0; i < name.length(); i++)
-			if (MatchmakingServerInterface.ALLOWED_ROOM_CHARS.indexOf(name.charAt(i)) == -1)
-				return false;
-		return true;
-	}
+    /**
+     * Joins a standard chat room for the client. This method will find the first
+     * chat room that is not more than half full and join the client to it. If
+     * no such room exists, a new one will be created.
+     * @param client
+     */
+    public final static void joinStandardChatRoom(Client client) {
+        // <= so that if all rooms are 1/2 full it creates a new one
+        for (int i = 0; i <= chat_rooms.size(); i++) {
+            String room_name = "Chatroom" + i + 1;
+            ChatRoom room = getChatRoom(room_name);
+            // skip room if its already half full and make another
+            if (room.getUsers().size() > MatchmakingServerInterface.MAX_ROOM_USERS / 2) {
+                continue;
+            }
 
-	public final void join(Client client) {
-		// TODO check for size!!!!
-		users.add(client);
-		sendUsers();
-	}
+            client.joinRoom(room_name);
+            return; // Client has joined a room
+        }
+    }
 
-	public final void sendUsers() {
-		Iterator it = users.iterator();
-		ChatRoomUser[] chat_room_users = new ChatRoomUser[users.size()];
-		int i = 0;
-		while (it.hasNext()) {
-			Client client = (Client)it.next();
-			chat_room_users[i] = new ChatRoomUser(client.getProfile().getNick(), client.isPlaying());
-			i++;
-		}
-		it = users.iterator();
-		while (it.hasNext()) {
-			Client client = (Client)it.next();
-			client.getClientInterface().receiveChatRoomUsers(chat_room_users);
-		}
-	}
+    /**
+     * Gets a chat room by name, creating it if it does not exist.
+     * @param room_name
+     * @return
+     */
+    public final static ChatRoom getChatRoom(String room_name) {
+        ChatRoom room = (ChatRoom) chat_rooms.get(room_name);
+        if (room == null) {
+            room = new ChatRoom(room_name);
+            chat_rooms.put(room_name, room);
+        }
+        return room;
+    }
 
-	public final void sendMessage(String msg, String owner) {
-		Iterator it = users.iterator();
-		while (it.hasNext()) {
-			Client client = (Client)it.next();
-			client.getClientInterface().receiveChatRoomMessage(msg, owner);
-		}
-	}
+    public final static boolean isNameValid(String name) {
+        return name != null && name.length() <= MatchmakingServerInterface.MAX_ROOM_NAME_LENGTH
+                && name.length() >= MatchmakingServerInterface.MIN_ROOM_NAME_LENGTH && areCharactersValid(name);
+    }
 
-	public final Set getUsers() {
-		return users;
-	}
+    private final static boolean areCharactersValid(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            if (MatchmakingServerInterface.ALLOWED_ROOM_CHARS.indexOf(name.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public final void leave(Client client) {
-		if (users.contains(client)) {
-			users.remove(client);
-			if (users.size() == 0) {
-				chat_rooms.remove(getName());
-			} else {
-				sendUsers();
-			}
-		}
-	}
+    public final void join(Client client) {
+        // TODO check for size!!!!
+        users.add(client);
+        sendUsers();
+    }
 
-	public final String getName() {
-		return name;
-	}
+    public final void sendUsers() {
+        Iterator it = users.iterator();
+        ChatRoomUser[] chat_room_users = new ChatRoomUser[users.size()];
+        int i = 0;
+        while (it.hasNext()) {
+            Client client = (Client) it.next();
+            chat_room_users[i] = new ChatRoomUser(client.getProfile().getNick(), client.isPlaying());
+            i++;
+        }
+        it = users.iterator();
+        while (it.hasNext()) {
+            Client client = (Client) it.next();
+            client.getClientInterface().receiveChatRoomUsers(chat_room_users);
+        }
+    }
+
+    /**
+     * Sends a message to all connected users in the chat room.
+     * @param owner
+     * @param msg
+     */
+    public final void sendMessage(String owner, String msg) {
+        for (Client client : users) {
+            MatchmakingClientInterface ci = client.getClientInterface();
+            ci.receiveChatRoomMessage(owner, msg);
+        }
+    }
+
+    /**
+     * Sends a discord message into the discord channel associated with this
+     * tribal trouble chat room
+     */
+    public final void trySendDiscordMessage(String owner, String msg) {
+        // Send the message to the discord channel if one is setup for this chat room
+        if (this.discordChannel != null) {
+            this.discordChannel.createMessage(msg).subscribe();
+        }
+    }
+
+    public final Set getUsers() {
+        return users;
+    }
+
+    public final void leave(Client client) {
+        if (users.contains(client)) {
+            users.remove(client);
+            if (users.size() == 0) {
+                chat_rooms.remove(getName());
+            } else {
+                sendUsers();
+            }
+        }
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    /**
+     * Sends a message to the chat room associated with the discord channel that the message was sent through
+     * @param event The message event containing the message details from discord
+     * @return
+     */
+    private Mono<Void> handleIncomingDiscordMessage(MessageCreateEvent event) {
+        System.err.println("handling on chatroom" + name);
+        if (this.discordChannel != null) {
+            Message message = event.getMessage();
+
+            if (message.getChannelId().equals(discordChannel.getId())) {
+                System.out.println("Message in channel tt discord channel posted");
+                // Handle the message
+                String content = message.getContent();
+                Instant timestamp = message.getTimestamp();
+                Snowflake authorId = message.getAuthor().get().getId();
+                boolean isMessageFromBot = authorId.equals(DiscordBotService.getInstance().getBotId());
+                if (!isMessageFromBot) {
+                    System.out.println("Message from " + authorId + ": " + content + " at " + timestamp);
+                    String author = "@" + message.getAuthor().map(user -> user.getUsername()).orElse("Unknown");
+                    System.out.println("Queueing Discord message for main thread processing");                
+                    sendMessage(author, content);
+                }
+            }
+        }
+
+        return Mono.empty();
+    }
 }
--- server/classes/com/oddlabs/matchserver/DiscordBotService.java
+++ server/classes/com/oddlabs/matchserver/DiscordBotService.java
@@ -1,19 +1,20 @@
 package com.oddlabs.matchserver;
 
-import java.time.Instant;
+import java.util.ArrayList;
 
 import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
-import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.object.entity.channel.TextChannel;
+import discord4j.core.object.entity.channel.Channel;
+
 import reactor.core.publisher.Mono;
 
 public class DiscordBotService {
 
+    Snowflake bot_id;
     private GatewayDiscordClient gateway;
     private static DiscordBotService instance;
 
@@ -34,7 +35,7 @@ public class DiscordBotService {
         Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> {
             this.gateway = gateway;
             // Extra discord things that can be done
-
+            bot_id = gateway.getSelfId();
             // Get connected servers
             gateway.getGuilds().subscribe(guild -> {
                 System.out.printf("Connected to guild: %s (ID: %s)%n", guild.getName(), guild.getId().asString());
@@ -46,23 +47,63 @@ public class DiscordBotService {
         login.subscribe();
     }
 
-    private void setupEventHandlers() {
+    /**
+     * Gets the discord id associated with the bot user
+     */
+    Snowflake getBotId() {
+        return bot_id;
+    }
 
+    private ArrayList<TextChannel> message_channels = new ArrayList<TextChannel>();
+
+    /**
+     * Sets up event handlers for the Discord bot
+     */
+    private void setupEventHandlers() {
+        initTribalTroubleTextChannels();
         gateway.on(ReadyEvent.class, this::handleReady).subscribe();
-        gateway.on(MessageCreateEvent.class, this::handleMessage).subscribe();
-        // Add more event handlers as needed
     }
 
     public GatewayDiscordClient getGateway() {
         return gateway;
     }
 
-    public void sendMessage(String channelId, String message) {
+    /**
+     * Gets the discord chatroom that is associated with the TT chat room based
+     * off the tribal trouble chat room number
+     */
+    public TextChannel getDiscordChannelByTTRoomNumber(int roomNumber) {
+        if (roomNumber < 1 || roomNumber > message_channels.size()) {
+            return null;
+        }
+        return message_channels.get(roomNumber - 1);
+    }
+
+    /**
+     * Initializes the message channels that can be used to relay messages to
+     * and from discord and tribal trouble. Currently the discord channels must
+     * be name tt_chatroom_<number> where <number> is the same as the tribal
+     * trouble chat room number
+     */
+    private void initTribalTroubleTextChannels() {
         if (gateway != null) {
-            gateway.getChannelById(Snowflake.of(channelId))
-                    .cast(MessageChannel.class)
-                    .flatMap(channel -> channel.createMessage(message))
-                    .subscribe();
+            gateway.getGuilds()
+                    .doOnNext(guild -> System.out.printf("Channels in guild: %s%n", guild.getName()))
+                    .flatMap(guild -> guild.getChannels())
+                    .subscribe(channel -> {
+
+                        System.out.printf("  - %s (ID: %s, Type: %s)%n",
+                                channel.getName(),
+                                channel.getId().asString(),
+                                channel.getType().name());
+
+                        if (channel.getType() == Channel.Type.GUILD_TEXT) {
+                            if (channel.getName().indexOf("tt_chatroom_") != -1) {
+                                message_channels.add((TextChannel) channel);
+                                System.out.println("Added channel: " + channel.getName());
+                            }
+                        }
+                    });
         }
     }
 
@@ -72,20 +113,4 @@ public class DiscordBotService {
         System.out.printf("Logged in as %s#%n", self.getUsername());
         return Mono.empty();
     }
-
-    private Mono<Void> handleMessage(MessageCreateEvent event) {
-        Message message = event.getMessage();
-        String content = message.getContent();
-        Instant timestamp = message.getTimestamp();
-        String author = message.getAuthor().map(user -> user.getUsername()).orElse("Unknown");
-        message.getAuthorAsMember()
-                .map(member -> member.getNickname().orElse(member.getUsername()))
-                .subscribe(nickname -> {
-                    System.out.printf("Nickname: %s%n", nickname);
-                });
-        System.out.printf("Message from %s: %s (at %s)%n", author, content, timestamp);
-
-        return Mono.empty();
-    }
-
 }
