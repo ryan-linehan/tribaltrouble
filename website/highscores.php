04303a41c888e649037f2d409f749a9e8c2cadfe
Omar Mokhtar <omar.ali.mokhtar@gmail.com>
2025-08-08 20:54:34 +0200

    Issue #54: Fixing a stalling problem during live streaming

--- website/highscores.php
+++ website/highscores.php
@@ -2,17 +2,17 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Cocur\HumanDate\HumanDate;
 use Carbon\Carbon;
+use Cocur\HumanDate\HumanDate;
 
-$servername = "localhost";
-$username = "matchmaker";
-$password = "U46TawOp";
-$dbname = "oddlabs";
+$servername = 'localhost';
+$username = 'matchmaker';
+$password = 'U46TawOp';
+$dbname = 'oddlabs';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+	die('Connection failed: ' . mysqli_connect_error());
 }
 
 $sql = "select * from games where status = 'completed'";
@@ -21,7 +21,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 	$scores = array();
 	while ($row = mysqli_fetch_assoc($result)) {
-		$winner_team = $row["winner"];
+		$winner_team = $row['winner'];
 		if ($winner_team == -1) {
 			continue;
 		}
@@ -41,7 +41,7 @@ if (mysqli_num_rows($result) > 0) {
 	foreach (array_reverse($scores) as $name => $score) {
 		echo "<tr><td class='pname'>$name</td><td class='pscore'>$score</td></tr>";
 	}
-	echo "</table>";
+	echo '</table>';
 }
 
 $sql = "SELECT * from games where time_stop = (select MAX(time_stop) from games where status='completed')";
@@ -49,66 +49,76 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 	$row = mysqli_fetch_assoc($result);
 	$humanDate = new HumanDate();
-	$time = $row["time_stop"];
+	$time = $row['time_stop'];
 	$last = $humanDate->transform($time);
-	if ($last == "Today") {
+	if ($last == 'Today') {
 		$date = Carbon::parse($time);
 		$diff = $date->diffForHumans();
 		$last = "$last $diff";
 	}
 	echo "<br>Last game played was <i>$last</i>.";
-	$winner = $row["winner"];
-	$names = "";
-	$losers = "";
+	$winner = $row['winner'];
+	$names = '';
+	$losers = '';
 	for ($i = 1; $i <= 8; $i++) {
 		$team = $row["player${i}_team"];
 		$name = $row["player${i}_name"];
 		if ($team == $winner) {
-			if ($names != "") {
+			if ($names != '') {
 				$names = "$names, $name";
 			} else {
 				$names = $name;
 			}
-		} else if ($name != "") {
-			if ($losers != "") {
+		} else if ($name != '') {
+			if ($losers != '') {
 				$losers = "$losers, $name";
 			} else {
 				$losers = $name;
 			}
 		}
 	}
-	if ($names != "") {
+	if ($names != '') {
 		echo " <b>$names</b> won";
-		if ($losers != "") {
+		if ($losers != '') {
 			echo " against <b>$losers</b>";
 		}
+		$id = $row['id'];
+		if (file_exists("/var/games/$id")) {
+			echo " <a href='watch.html#$id' target='new'>watch here</a>";
+		}
+		echo '</br>';
 	}
 }
 
-echo "<br/>";
-echo "<br/>";
+echo '<br/>';
+echo '<br/>';
 
 $sql = "SELECT * from games where status='started'";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
 		$players = array();
-		$name = $row["name"];
+		$name = $row['name'];
 		$num_players = 0;
 		for ($i = 1; $i <= 6; $i++) {
 			$player = $row["player${i}_name"];
-			if ($player != "") {
+			if ($player != '') {
 				$players[$num_players] = $player;
 				$num_players++;
 			}
 		}
+		$id = $row['id'];
+		$watch_here = '';
+		if (file_exists("/var/games/$id")) {
+			$watch_here = "<a href='watch.html#$id' target='new'>Watch it live here!</a>";
+		}
 		if ($num_players == 2) {
-			echo "<b>${players[0]}</b> and <b>${players[1]}</b> are playing <i>$name</i> now<br>";
+			echo "<b>${players[0]}</b> and <b>${players[1]}</b> are playing <i>$name</i> now $watch_here";
 		} else if ($num_players > 2) {
-			for ($i = 0 ; $i < $num_players - 1 ; $i++) {
+			for ($i = 0; $i < $num_players - 1; $i++) {
 				echo "<b>${players[$i]}</b>,";
 			}
-			echo " and <b>${players[$num_players-1]}</b> are playing <i>$name</i> now<br>";	
+			echo " and <b>${players[$num_players - 1]}</b> are playing <i>$name</i> now $watch_here";
 		}
 	}
 }
--- website/stream.php
+++ website/stream.php
@@ -1,10 +1,10 @@
 <?php
-$id = $_GET["gid"];
-$offset = $_GET["offset"];
+$id = $_GET['gid'];
+$offset = $_GET['offset'];
 $name = "/var/games/$id";
 $file = fopen($name, 'r');
 if ($file === false) {
-    die("Failed to open file");
+    die('Failed to open file');
 }
 $size = filesize($name);
 if ($offset < $size) {
