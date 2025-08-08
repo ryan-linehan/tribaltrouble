ae512b86fef05aee7f7ca19a066d77ec20786478
Omar Mokhtar <omar.ali.mokhtar@gmail.com>
2025-08-08 21:56:10 +0200

    Issue #56: Correction

--- ci/auto_format.sh
+++ ci/auto_format.sh
@@ -2,9 +2,4 @@
 
 HERE=$(realpath $(dirname $0))
 
-echo "These are the files I'm going to change"
-echo $(find $HERE/.. | grep "\.java$")
-
 $HERE/google-java-format_linux-x86-64 --aosp  -r $(find $HERE/.. | grep "\.java$")
-
-git diff --name-only
