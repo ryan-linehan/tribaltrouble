c76ac97e2e178ddb0a054c2d0e403e759ecb8c74
Omar Mokhtar <omar.ali.mokhtar@gmail.com>
2025-08-21 01:00:52 +0200

    Issue #62: Deleting unused file

--- website/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function showPlayer($name) {
-    return "</b><a href='index.html#player#$name'>$name</a></b>";
-}
-
-function showGame($name, $gid) {
-    return "<i>$name</i><a href='watch.html#$gid' target='new'>Watch Here!</a>";
-}
--- website/gamesOf.php
+++ website/gamesOf.php
@@ -1,7 +1,5 @@
 <?php
 
-include 'common.php';
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Carbon\Carbon;
